7d46e1181e85565d6952d123f29508837d04e018
Jeremy Faraone <jeremyfaraone@gmail.com>
2020-08-09 16:53:10 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Route } from 'react-router-dom';
-import MovieList from './Movies/MovieList';
-import SavedList from './Movies/SavedList';
-import Movie from './Movies/Movie';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import MovieList from "./Movies/MovieList";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
-	const [savedList, setSavedList] = useState([]);
+  const [savedList, setSavedList] = useState([]);
 
-	const addToSavedList = (movie) => {
-		setSavedList([...savedList, movie]);
-	};
+  const addToSavedList = (movie) => {
+    setSavedList([...savedList, movie]);
+  };
 
-	return (
-		<div>
-			<SavedList list={savedList} />
-			<Route exact path="/" component={MovieList} />
-			<Route path="/movies/:id" component={Movie} />
-		</div>
-	);
+  return (
+    <div>
+      <SavedList list={savedList} />
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
+    </div>
+  );
 };
 
 export default App;
